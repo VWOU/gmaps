e6adbce07623b0d9a28aada766297e696bf16b1e
VWOU <vincent.wouters@gmail.com>
2013-02-12 14:32:35 +0100

    Added Front Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Gmaps::Application.routes.draw do
+ 
+  root :to => 'home#index'      
   resources :characters
 
 
@@ -51,7 +53,7 @@ Gmaps::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'characters#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
